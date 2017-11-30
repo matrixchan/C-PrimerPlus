bb8c690bdcf544b88f03dcd964900814fbb1c9fa
John <john@JohndeMacBook-Air.local>
2017-11-30 19:01:09 +0800

    first commit

---
+++ ch02/myfirst.cpp
@@ -0,0 +1,9 @@
+//
+//  myfirst.cpp
+//  CppPrimerPlus
+//
+//  Created by John on 28/11/2017.
+//  Copyright © 2017 John. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ ch04/pactice_01.cpp
@@ -0,0 +1,9 @@
+//
+//  pactice_01.cpp
+//  CppPrimerPlus
+//
+//  Created by John on 29/11/2017.
+//  Copyright © 2017 John. All rights reserved.
+//
+
+#include <stdio.h>
